7f9de3c98c4c5f875ddc5157e1f3a10f081a4d9d
Jonathan Conger <44207939+jonathanconger@users.noreply.github.com>
2022-11-20 21:02:36 -0700

    trail renderer and wind hazard

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
 	//[SerializeField] public GameObject hitEffect;
-	private Rigidbody2D rb;
+	[SerializeField] private Rigidbody2D rb;
 
-	private void Awake()
+	private void FixedUpdate()
 	{
-		rb = GetComponent<Rigidbody2D>();
+		// rotate the arrow to the direction it is moving in
+		Vector2 rotateVector = new Vector2(rb.velocity.x, rb.velocity.y);
+		if (rotateVector != Vector2.zero)
+		{
+			transform.rotation = Quaternion.LookRotation(Vector3.forward, rotateVector);
+		}
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
--- Assets/Scripts/Level/WindHazard.cs
+++ Assets/Scripts/Level/WindHazard.cs
@@ -6,18 +6,18 @@ public class WindHazard : MonoBehaviour
 	[SerializeField] public bool affectsArrows = true;
     [SerializeField] public bool affectsPlayer = false;
 
-	private void OnTriggerEnter2D(Collider2D collision)
+	private void OnTriggerStay2D(Collider2D collision)
 	{
 		// wind for arrows
 		if (affectsArrows && collision.CompareTag("Arrow"))
 		{
-			collision.GetComponent<Rigidbody2D>().AddForce(transform.forward * windSpeed, ForceMode2D.Impulse);
+			collision.GetComponent<Rigidbody2D>().AddForce(transform.up * windSpeed * Time.deltaTime, ForceMode2D.Impulse);
 		}
 
 		// wind for player
 		if (affectsPlayer && collision.CompareTag("Player"))
 		{
-			collision.GetComponent<Rigidbody2D>().AddForce(transform.forward * windSpeed, ForceMode2D.Impulse);
+			collision.GetComponent<Rigidbody2D>().AddForce(transform.up * windSpeed * Time.deltaTime, ForceMode2D.Impulse);
 		}
 	}
 }
